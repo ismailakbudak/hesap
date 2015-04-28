c06a6e1f3318f55478d7d1f6c485053ac6ffe188
ismail Akbudak <isoakbudak@gmail.com>
2015-04-29 01:39:07 +0300

    rspec test writed for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe User do
+
+  before :each do
+    @user = User.new email: 'test@test.com', password: '12341234', name: 'test'
+  end
+
+  describe '#new' do
+    it 'takes two parameters an returns  a User object' do
+      @user.should be_an_instance_of User
+    end
+  end
+
+  describe '#email' do
+    it 'returns the correct email' do
+      @user.email.should eql 'test@test.com'
+    end
+  end
+
+  describe '#password' do
+    it 'returns the correct password' do
+      @user.password.should eql '12341234'
+    end
+  end
+
+  describe '#name' do
+    it 'returns the correct name' do
+      @user.name.should eql 'test'
+    end
+  end
+
 end
 
